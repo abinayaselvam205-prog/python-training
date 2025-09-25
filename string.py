c5b14eb23feba016220a41b542c8b97753303d64
abinayaselvam205-prog <abinayaselvam205@gmail.com>
2025-09-25 11:45:57 +0530

    25/09/2025
    pthon sample in string

---
+++ string.py
@@ -0,0 +1,9 @@
+text="welcome to abinaya in python program"
+print("length:",len(text))
+print("split:",text.split())
+print("replace:",text.replace("python","java"))
+print("lower:",text.lower())
+print("upper:",text.upper())
+print("find:",text.find("in"))
+print("startswith 'python':",text.startswith("python"))
+print("endswith 'program':",text.endswith("program"))
